83ca794a2ce9f47267029da508cf9291a709ee6d
SvenMayCry <svenmaycry@gmail.com>
2023-09-16 16:43:05 +0300

    feat: Добавит картинки + тест стейта

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <div className="header">
       <div className="container">
         <div className="header__logo">
-          <img width={38} src="./img/pizza-logo.svg" alt="Pizza logo" />
+          <img width={38} src="images/pizza-logo.svg" alt="Pizza logo" />
           <div>
             <h1>React Pizza</h1>
             <p>самая вкусная пицца во вселенной</p>
@@ -18,7 +18,8 @@ const Header = () => {
               height={18}
               viewBox="0 0 18 18"
               fill="none"
-              xmlns="http://www.w3.org/2000/svg">
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
                 d="M6.33333 16.3333C7.06971 16.3333 7.66667 15.7364 7.66667 15C7.66667 14.2636 7.06971 13.6667 6.33333 13.6667C5.59695 13.6667 5 14.2636 5 15C5 15.7364 5.59695 16.3333 6.33333 16.3333Z"
                 stroke="white"
