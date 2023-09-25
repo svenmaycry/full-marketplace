b7910586507e201ef5d365bf7d29580bc29f2e58
SvenMayCry <svenmaycry@gmail.com>
2023-09-25 21:33:41 +0300

    feat: Добавит функционал popup-окна сортировки

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -1,4 +1,16 @@
+import { useState } from 'react';
+
 const Sort = () => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  const list = ['цене', 'популярности', 'алфавиту'];
+  const sortName = list[activeItem];
+
+  const onListItemClick = (idx: number) => {
+    setActiveItem(idx);
+    setOpen(false);
+  };
+
   return (
     <div className="sort">
       <div className="sort__label">
@@ -7,22 +19,31 @@ const Sort = () => {
           height={6}
           viewBox="0 0 10 6"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg">
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
             d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
             fill="#2C2C2C"
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setOpen(!open)}>{sortName}</span>
       </div>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {open && (
+        <div className="sort__popup">
+          <ul>
+            {list.map((value, idx) => (
+              <li
+                onClick={() => onListItemClick(idx)}
+                className={activeItem === idx ? 'active' : ''}
+                key={idx}
+              >
+                {value}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
