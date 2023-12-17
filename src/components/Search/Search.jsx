6384e1fd45905a4872d45d170e65c3afc9f5b781
SvenMayCry <svenmaycry@gmail.com>
2023-12-17 14:51:32 +0300

    feat: Поправит фокус в инпуте. Используем useRef()

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,15 @@
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import styles from './Search.module.scss';
 import { SearchContext } from '../App/App';
 
 export const Search = () => {
   const { searchValue, setSearchValue } = useContext(SearchContext);
+  const inputRef = useRef();
+
+  const onClickClear = () => {
+    setSearchValue('');
+    inputRef.current.focus();
+  };
 
   return (
     <div className={styles.root}>
@@ -18,6 +24,7 @@ export const Search = () => {
         <path d="M464,428,339.92,303.9a160.48,160.48,0,0,0,30.72-94.58C370.64,120.37,298.27,48,209.32,48S48,120.37,48,209.32s72.37,161.32,161.32,161.32a160.48,160.48,0,0,0,94.58-30.72L428,464ZM209.32,319.69A110.38,110.38,0,1,1,319.69,209.32,110.5,110.5,0,0,1,209.32,319.69Z" />
       </svg>
       <input
+        ref={inputRef}
         value={searchValue}
         onChange={(evt) => setSearchValue(evt.target.value)}
         className={styles.input}
@@ -25,7 +32,7 @@ export const Search = () => {
       />
       {searchValue && (
         <svg
-          onClick={() => setSearchValue('')}
+          onClick={onClickClear}
           className={styles.close}
           data-name="Layer 1"
           height="200"
