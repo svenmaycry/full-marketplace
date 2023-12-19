d827b5fa95ec2bace4f70dd28430cc69e5606fcf
SvenMayCry <svenmaycry@gmail.com>
2023-12-19 18:13:38 +0300

    feat: Добавит логику на показ / скрытие попап окна сортировки, при клике на body.

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setSort } from '../../redux/slices/filterSlice.js';
 
@@ -14,6 +14,7 @@ export const sortList = [
 export const Sort = () => {
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.filter.sort);
+  const sortRef = useRef();
 
   const [open, setOpen] = useState(false);
 
@@ -22,8 +23,21 @@ export const Sort = () => {
     setOpen(false);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.composedPath().includes(sortRef.current)) {
+        setOpen(false);
+      }
+    };
+    document.body.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.body.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width={10}
