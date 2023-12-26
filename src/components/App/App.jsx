b94233365e0dc2d867d4e13310fb03eac4a3ae08
SvenMayCry <svenmaycry@gmail.com>
2023-12-26 19:25:37 +0300

    feat: Используем <Outlet/> для динамических роутов.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,21 @@
 import '../../scss/app.scss';
 
-import { Header } from '../Header/Header';
 import { Home } from '../../pages/Home/Home';
 import { NotFound } from '../../pages/NotFound/NotFound';
 import { Cart } from '../../pages/Cart/Cart';
 import { Routes, Route } from 'react-router-dom';
 import { FullFlower } from '../../pages/FullFlower/FullFlower';
+import { MainLayout } from '../Layouts/MainLayout';
 
 export const App = () => {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/flower/:id" element={<FullFlower />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="flower/:id" element={<FullFlower />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 };
